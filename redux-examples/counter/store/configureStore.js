69f7dbc2e58a339a1a6495801bc3fb34488ee188
lewis617 <897473127@qq.com>
2016-02-02 17:21:31 +0800

    chrome extend

--- redux-examples/counter/store/configureStore.js
+++ redux-examples/counter/store/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware,compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from '../reducers'
 
 //applyMiddleware来自redux可以包装 store 的 dispatch
 //thunk作用是使被 dispatch 的 function 会接收 dispatch 作为参数，并且可以异步调用它
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
+const createStoreWithMiddleware = compose(
+    applyMiddleware(
+        thunk
+    ),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)
 
 export default function configureStore(initialState) {
--- redux-examples/todomvc/store/configureStore.js
+++ redux-examples/todomvc/store/configureStore.js
@@ -1,8 +1,12 @@
-import { createStore } from 'redux'
+import { createStore ,compose} from 'redux'
 import rootReducer from '../reducers'
 
+const finalCreateStore = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState)
+  const store = finalCreateStore(rootReducer, initialState);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
